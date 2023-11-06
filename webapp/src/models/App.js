5de06f35f88a7920f5e4e82a9cfcba8abc8c9cee
Mwni <manuel@tigmar.de>
2023-11-06 12:02:17 +0100

    log websocket status to console

--- webapp/src/models/App.js
+++ webapp/src/models/App.js
@@ -1,9 +1,20 @@
 import createSocket from '@mwni/socket'
 import { createEmitter } from '@mwni/events'
 
-export default class extends createEmitter{
+export default class{
 	constructor({ backendUrl }){
-		super()
+		Object.assign(this, createEmitter())
+		
 		this.socket = createSocket({ url: backendUrl })
+		this.socket.on('connect', this.onConnect.bind(this))
+		this.socket.on('disconnect', this.onDisconnect.bind(this))
+	}
+
+	onConnect(){
+		console.log('connection to backend established')
+	}
+
+	onDisconnect(){
+		console.warn('connection to backend lost')
 	}
 }
