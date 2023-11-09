a26e0c662626eb6621f125e1ae7c498ec9a43952
Mwni <manuel@tigmar.de>
2023-11-09 11:47:24 +0100

    tidy landing page

---
+++ webapp/src/components/landing/landing-page.svelte
@@ -0,0 +1,85 @@
+<script>
+	import URlogo from '../../assets/svg/URlogo.svelte'
+
+</script>
+
+<div class="landing-container">
+	<div class="logo-UR">
+		<URlogo/>
+	</div>
+	<div class="welcome-message">
+		<div class="label">Welcome to the study</div>
+		<div class="subtext">You will be brainstorming with AI</div>
+	</div>
+	<div class="code-input-container">
+		<input type="text" id="registration-code" placeholder="Your Registration Code">
+		<div class="instruction">Once we start, please enter your registration code</div>
+	</div>
+</div>
+
+
+<style lang="scss">
+.landing-container{
+	display: flex;
+	flex-direction:column;
+	width: 100%;
+	height: 100%;
+}
+
+.logo-UR{
+	display: flex;
+	width: auto;
+	margin-left: auto;
+	margin-right: 25px;
+	margin-top: 25px;
+}
+
+.welcome-message{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 10%;
+	gap: 5px;
+	color: #34E5B0;
+
+	.label{
+		font-size: 36px;
+		font-weight: bold;
+	}
+}
+
+.code-input-container{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 5%;
+
+	input{
+		border: none;
+		height: 44px;
+		width: 380px;
+		background-color: transparent;
+		border-bottom: solid #CACACA;
+		justify-content: center;
+		outline: none;
+		color: #FEFEFE;
+		text-align: center;
+		font-size: 24px;
+		font-family: inherit;
+		font-weight: bold;
+
+		&::placeholder {
+			text-align: center;
+			font-size: 24px;
+			font-weight: bold;
+			color: #3E3E3E;
+		}
+	}
+
+	.instruction{
+		margin-top: 10px;
+		color: #959595;
+	}
+}
+
+</style>
