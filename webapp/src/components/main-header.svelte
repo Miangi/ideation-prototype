f7a384dbf921d9f908dfe99d55efb7f03cc7dc5b
Mwni <manuel@tigmar.de>
2023-11-15 17:49:37 +0100

    add display for online users

--- webapp/src/components/main-header.svelte
+++ webapp/src/components/main-header.svelte
@@ -2,7 +2,7 @@
 	import LogoSmall from '../assets/svg/logo_small.svelte'
 
 	import { onMount } from 'svelte'
-    import { currentTask, userMeta } from '../models/app.js'
+    import { currentTask, userMeta, users } from '../models/app.js'
 
 	let time
 
@@ -36,28 +36,37 @@
 					Loading Task ...
 				{/if}
 			</div>
-			<div class="participant-info">
-				<div class="participant-info-name">
-					{#if $userMeta}
-						<div class="name">{$userMeta.firstName}</div>
-						<div class="surname">{$userMeta.lastName}</div>
-						<div class="id">#{$userMeta.id}</div>
-					{:else}
-						<div class="name">name</div>
-						<div class="surname">surname</div>
-						<div class="id">#id</div>
-					{/if}
+			<div class="righthand">
+				<div class="online">
+					{#each $users as user}
+						<div class="bubble" title={`${user.firstName} ${user.lastName}`}>
+							{user.firstName.slice(0, 1).toUpperCase() + user.lastName.slice(0, 1).toUpperCase()}
+						</div>
+					{/each}
 				</div>
-				<div class="time-and-date">{time}</div>
-				<div class="assigned-group">
-					{$userMeta?.team?.name}
+				<div class="participant-info">
+					<div class="participant-info-name">
+						{#if $userMeta}
+							<div class="name">{$userMeta.firstName}</div>
+							<div class="surname">{$userMeta.lastName}</div>
+							<div class="id">#{$userMeta.id}</div>
+						{:else}
+							<div class="name">name</div>
+							<div class="surname">surname</div>
+							<div class="id">#id</div>
+						{/if}
+					</div>
+					<div class="time-and-date">{time}</div>
+					<div class="assigned-group">
+						{$userMeta?.team?.name}
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 
-<style>
+<style lang="scss">
 	.background-container-header {
 		display: flex;
 	}
@@ -84,10 +93,34 @@
 		color: #9ca4a9;
 	}
 
+	.righthand{
+		display: flex;
+		align-items: center;
+		gap: 10px;
+		margin-left: auto;
+	}
+
+	.online{
+		display: flex;
+		gap: 5px;
+
+		.bubble{
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 30px;
+			height: 30px;
+			border-radius: 100px;
+			background-color: #191E49;
+			color: #4F87DB;
+			font-size: 12px;
+			cursor: default;
+		}
+	}
+
 	.participant-info {
 		display: flex;
 		flex-direction: column;
-		margin-left: auto;
 		gap: 7px;
 		min-height: 65px;
 	}
