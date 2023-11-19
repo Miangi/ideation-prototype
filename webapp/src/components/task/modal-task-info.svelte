b29baef2c69d196b490184bedcdbadf22ed22b9b
Michael <michael.gindert@gmail.com>
2023-11-19 19:06:25 +0100

    Adds final task backgrounds and tasks/subtasks

--- webapp/src/components/task/modal-task-info.svelte
+++ webapp/src/components/task/modal-task-info.svelte
@@ -10,7 +10,7 @@
         <div class="close-button" on:click={() => $visibleModals.taskInfo = false}><MinimizeTaskInfo/></div>
         <div class="title">Information</div>
 		{#if $currentTask?.number === 1}
-			<div class="info1"><p>There's been a significant increase in the global aging population. The world will see the numbers grow from 761 million today to 1.6 billion in 2050. Likewise, the population aged 80 years and older will also grow exponentially. With it, there are serious health issues, such as chronic illnesses and mobility-related accidents, that need to be addressed. At the same time, there is a rising demand for innovative products and services to help seniors maintain quality of life, be more independent, and have a societal connection. The market for healthcare solutions targeting seniors is in its infancy, with ample room for growth and innovative breakthroughs. Your task is to tap into this potential.
+			<div class="info1"><p>There's been a significant increase in the global aging population. The world will see the numbers from people aged 60 or older grow from 761 million today to 1.6 billion in 2050. Likewise, the population aged 80 years and older will also grow exponentially. With it, there are serious health issues, such as chronic illnesses and mobility-related accidents, that need to be addressed. At the same time, there is a rising demand for innovative products and services to help seniors maintain quality of life, be more independent, and have a societal connection. The market for healthcare solutions targeting seniors is in its infancy, with ample room for growth and innovative breakthroughs. Your task is to tap into this potential.
 				<br>
 				<br>
 				Now keep in mind that the older community faces a myriad of healthcare obstacles, including chronic diseases, mobility hurdles, and cognitive degeneration. The better the solutions we can create to address these, the more we can enhance seniors' living standards. With technological advancements in wearable devices, telehealth services, and remote monitoring systems, healthcare for the elderly can be transformed and improved. These innovative possibilities push the boundaries of more accessible care, early health issue detection, and better monitoring.</p>
@@ -18,8 +18,9 @@
 		{:else if $currentTask?.number === 2}
 			<div class="info2"><p>The transportation sector is on the brink of a major revolution with the introduction of autonomous vehicles. This shift goes beyond traditional car manufacturers and includes technology companies, startups, and service providers, all of whom have the potential to transform the industry with new business models centered around autonomous driving technologies.
 				Cloud-based computational technologies are playing an increasingly significant role in this transformation, providing immense storage capacities and powerful processing capabilities to support autonomous systems. When combined with artificial intelligence, these technologies enable vehicles to learn, make real-time decisions, and adapt to their surroundings.
-				Changing trends indicate a shift from the traditional model of owning a car to a more flexible "use-as-needed" approach, which is shaping the business models in this domain.
-				To adapt to these evolving trends, businesses need to address new customer needs and preferences. While safety and comfort remain important, factors such as on-board entertainment and maximizing travel time productivity are gaining prominence, driving innovation in the industry.</p>
+				<br>
+				<br>
+				The shift to autonomous driving creates opportunities for innovative business models, such as Mobility as a Service (MaaS) and shared ownership. Eliminating the need for parking opens up space for revenue generation. In-car entertainment and productivity services can enhance the travel experience. Autonomous delivery services and insurance models may also evolve. Infrastructure upgrades will be necessary for communication and data-sharing systems.</p>
 			</div>
 		{:else}
 			Loading task info ...
