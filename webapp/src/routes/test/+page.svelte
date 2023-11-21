9bcb7e5341d5264e13e95032cf88cdc57de5f6d8
Michael <michael.gindert@gmail.com>
2023-11-21 14:59:55 +0100

    deleting unessecary test route

--- webapp/src/routes/test/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Test1 from '../../components/task/modal-confirm-task-solution.svelte'
-    import Test2 from '../../components/task/modal-task-info.svelte'
-
-</script>
- 
-
-<Test1/>
-<Test2/>
